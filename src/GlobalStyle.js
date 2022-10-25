695272f6067f807a388237469bbc6157c97973be
Stefan Kondring <kondring.s@gmx.de>
2022-10-25 10:27:15 +0200

    implement "Cookie"-font for h1

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,22 +1,34 @@
 import { createGlobalStyle } from "styled-components";
 import NotoSans from "./fonts/noto-sans-v27-latin-regular.woff";
 import NotoSans2 from "./fonts/noto-sans-v27-latin-regular.woff2";
+import Cookie from "./fonts/Cookie-Regular.ttf";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
+//Main-font:
     font-family: 'Noto Sans';
-    src: url(${NotoSans2}) format('woff2'),
+    src:
+      local('Noto Sans'), 
+      url(${NotoSans2}) format('woff2'),
         url(${NotoSans}) format('woff');
+//Cookie-font (for the h1-header):
+    font-family: 'Cookie';
+    src: 
+    local("Cookie"),
+    url(${Cookie}) format('truetype');
   }
+
   * {
     box-sizing: border-box;
+    margin: 0;
   }
 
   body {
-    margin: 0;
-    font-family: 'Noto Sans', 'Segoe UI', 'Roboto', 'Oxygen',
+    --header-font: "Cookie", sans-serif;
+    --main-font: 'Noto Sans', 'Segoe UI', 'Roboto', 'Oxygen',
         'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
         sans-serif;
+    font-family: var(--main-font);
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
