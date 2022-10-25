dc5ebe2ba5d261cca406b58f3ffc224b787bfa27
Stefan Kondring <kondring.s@gmx.de>
2022-10-25 10:26:06 +0200

    add first elements
    - header
    - welcome message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import styled from "styled-components";
 
 function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <StyledHeader>
+        <h1>Cook4All</h1>
+      </StyledHeader>
+      <p>Welcome!</p>
+      <p>Soon you will see your guest list here!</p>
+    </Container>
+  );
 }
 
 const Container = styled.div`
   text-align: center;
+  margin: 0 auto;
+  max-width: 390px;
+  min-width: 320px;
+`;
+
+const StyledHeader = styled.header`
+  background-color: #dcedc1;
+  font-family: var(--header-font);
+  font-size: 2rem;
+  margin-bottom: 20px;
+  padding: 10px;
 `;
 
 export default App;
