fc66404f55350ad8d227989cac6957f2c1be8354
Stefan Kondring <kondring.s@gmx.de>
2022-10-25 12:26:36 +0200

    create page 'CreateGuest'
    - add heading
    - add simple form without functionality

---
+++ src/pages/CreateGuest.js
@@ -0,0 +1,12 @@
+export default function CreateGuest() {
+  return (
+    <>
+      <h2>Add a guest</h2>
+      <form>
+        <label>Name: </label>
+        <input type="text" />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+}
