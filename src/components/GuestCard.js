c9843a35668fd127c273d8bdaacb2cbfd2421c3d
Stefan Kondring <kondring.s@gmx.de>
2022-10-31 12:06:37 +0100

    add some icons

--- src/components/GuestCard.js
+++ src/components/GuestCard.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../util/UserContext";
+import emptyCircle from "../assets/icons/circle_empty.svg";
+import checkedCircle from "../assets/icons/circle-check.svg";
+import detailIcon from "../assets/icons/more-menu-vertical.svg";
 
 export default function GuestCard({ personalData }) {
   const { name, id, selected } = personalData;
@@ -25,11 +28,15 @@ export default function GuestCard({ personalData }) {
   return (
     <StyledArticle>
       <StyledCheckButton type="button" onClick={() => toggleSelected(id)}>
-        {selected === true ? "On" : "Off"}
+        {selected === true ? (
+          <img src={checkedCircle} alt="selected" />
+        ) : (
+          <img src={emptyCircle} alt="unselected" />
+        )}
       </StyledCheckButton>
       <p>{name}</p>
       <StyledDetailsButton onClick={() => navigate(`/details/${id}`)}>
-        details
+        <img src={detailIcon} alt="details" />
       </StyledDetailsButton>
     </StyledArticle>
   );
@@ -39,6 +46,7 @@ const StyledArticle = styled.article`
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-start;
+  align-items: center;
   padding: 10px;
   margin: 10px auto;
   max-width: 80%;
@@ -50,6 +58,7 @@ const StyledArticle = styled.article`
     display: inline;
     margin-left: 20px;
     word-wrap: break-word;
+    font-size: 1.3rem;
   }
 
   input {
@@ -61,8 +70,28 @@ const StyledDetailsButton = styled.button`
   justify-self: flex-end;
   margin-left: auto;
   margin-right: 20px;
+  background-color: transparent;
+  border: unset;
+
+  img {
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.2);
+    }
+  }
 `;
 
 const StyledCheckButton = styled.button`
   margin-left: 20px;
+  margin-top: 5px;
+  padding: 0;
+  background-color: transparent;
+  border: unset;
+
+  img {
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.1);
+    }
+  }
 `;
