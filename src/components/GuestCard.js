be729157b74229e93c12613b0bf84b584e5e2bfc
Stefan Kondring <kondring.s@gmx.de>
2022-10-31 08:24:03 +0100

    add toggle-button
    - create and style toggle-button on GuestCard
    - add toggle functionality to (un-)select guests

--- src/components/GuestCard.js
+++ src/components/GuestCard.js
@@ -1,14 +1,36 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../util/UserContext";
 
 export default function GuestCard({ personalData }) {
-  const { name, id } = personalData;
+  const { name, id, selected } = personalData;
   const navigate = useNavigate();
+
+  const { guestArray, setGuestArray } = useContext(UserContext);
+
+  function toggleSelected(guestId) {
+    setGuestArray(
+      guestArray.map((guest) =>
+        guest.id === guestId
+          ? {
+              ...guest,
+              selected: !guest.selected,
+            }
+          : guest
+      )
+    );
+  }
+
   return (
     <StyledArticle>
-      <input type="checkbox" id={name} name="name" value={name} />
+      <StyledCheckButton type="button" onClick={() => toggleSelected(id)}>
+        {selected === true ? "On" : "Off"}
+      </StyledCheckButton>
       <p>{name}</p>
-      <button onClick={() => navigate(`/details/${id}`)}>details</button>
+      <StyledDetailsButton onClick={() => navigate(`/details/${id}`)}>
+        details
+      </StyledDetailsButton>
     </StyledArticle>
   );
 }
@@ -21,7 +43,7 @@ const StyledArticle = styled.article`
   margin: 10px auto;
   max-width: 80%;
   text-align: left;
-  background-color: var(--header-color-bg);
+  background-color: var(--primary-color);
   border-radius: 35px;
 
   p {
@@ -33,10 +55,14 @@ const StyledArticle = styled.article`
   input {
     margin-left: 20px;
   }
+`;
 
-  button {
-    justify-self: flex-end;
-    margin-left: auto;
-    margin-right: 20px;
-  }
+const StyledDetailsButton = styled.button`
+  justify-self: flex-end;
+  margin-left: auto;
+  margin-right: 20px;
+`;
+
+const StyledCheckButton = styled.button`
+  margin-left: 20px;
 `;
