c1c3aa1cc4dbdff1d52260a2bfc4512a69d023c7
Stefan Kondring <kondring.s@gmx.de>
2022-11-02 15:52:04 +0100

    adjust NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,22 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import homeIcon from "../assets/icons/home-line.svg";
 import recipesIcon from "../assets/icons/hot-meal-outline.svg";
+import favoritesIcon from "../assets/icons/heart-outlined.svg";
 
 export default function NavBar() {
   return (
     <StyledNavBar>
       <StyledNavLink to="/" end>
         <img src={homeIcon} alt="Go to Home" />
+        <p>Home</p>
       </StyledNavLink>
       <StyledNavLink to="/recipes">
         <img src={recipesIcon} alt="Go to Recipes" />
+        <p>Recipes</p>
+      </StyledNavLink>
+      <StyledNavLink to="/favorites">
+        <img src={favoritesIcon} alt="Go to favorite recipes" />
+        <p>Favorites</p>
       </StyledNavLink>
     </StyledNavBar>
   );
@@ -32,10 +39,17 @@ const StyledNavLink = styled(NavLink)`
   height: 60px;
   width: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  text-decoration: none;
 
   &.active {
     background-color: rgb(192, 218, 241);
+  }
+
+  p {
+    font-size: 0.9rem;
+    color: black;
   }
 `;
