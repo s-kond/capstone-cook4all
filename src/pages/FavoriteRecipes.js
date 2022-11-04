27328af3b8c8327d31eade6370a86bf2679addef
Stefan Kondring <kondring.s@gmx.de>
2022-11-04 16:42:39 +0100

    add component to FavoriteRecipes
    - create new filter logic

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,19 +1,50 @@
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import styled from "styled-components";
+import DisplaySelectedGuests from "../components/DisplaySelectedGuests";
 import NavBar from "../components/NavBar";
 import RecipeCard from "../components/RecipeCard";
+import getIntolerances from "../util/GetSelectedGuestsIntolerances";
 import { UserContext } from "../util/UserContext";
 import { StyledHeader } from "./Home";
 
 export default function FavoriteRecipes() {
-  const { favoriteArray } = useContext(UserContext);
+  const { favoriteArray, guestArray } = useContext(UserContext);
+  const [filteredFavorites, setFilteredFavorites] = useState(favoriteArray);
+  const intolerances = getIntolerances(guestArray);
+  const upperCaseIntolerances = intolerances.map(
+    (item) => item[0].toUpperCase() + item.slice(1)
+  );
+
+  useEffect(() => {
+    setFilteredFavorites(
+      favoriteArray.filter((item) =>
+        upperCaseIntolerances.every((intolerance) =>
+          item.recipe.healthLabels.includes(intolerance)
+        )
+      )
+    );
+  }, []);
+
   return (
     <>
       <StyledHeader>
         <h2>Favorites</h2>
       </StyledHeader>
+      <DisplaySelectedGuests />
+      <hr />
       <StyledFavoriteSection>
-        {favoriteArray.map((favorite) => (
+        {filteredFavorites.length === 0 ? (
+          <>
+            <p>
+              Unfortunately, there are no favorite recipes <br /> for all of
+              your guests... :(
+            </p>
+            <p>Go to the recipe page and find new favorites!</p>
+          </>
+        ) : (
+          ""
+        )}
+        {filteredFavorites.map((favorite) => (
           <RecipeCard key={favorite.recipe.uri} recipeData={favorite} />
         ))}
       </StyledFavoriteSection>
@@ -24,4 +55,8 @@ export default function FavoriteRecipes() {
 
 const StyledFavoriteSection = styled.section`
   margin-bottom: 100px;
+  p:first-of-type {
+    margin-top: 70px;
+    margin-bottom: 10px;
+  }
 `;
