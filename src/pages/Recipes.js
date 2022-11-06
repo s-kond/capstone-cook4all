c8703914bc6d6e0521810586f9dbc75463a03717
Stefan Kondring <kondring.s@gmx.de>
2022-11-06 14:05:37 +0100

    add ErrorBoundary, next page results

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -2,6 +2,7 @@ import { StyledHeader } from "./Home";
 import { useState, useContext } from "react";
 import { UserContext } from "../util/UserContext";
 import styled from "styled-components";
+import { ErrorBoundary } from "react-error-boundary";
 import NavBar from "../components/NavBar";
 import RecipeCard from "../components/RecipeCard";
 import getIntolerances from "../util/GetSelectedGuestsIntolerances";
@@ -10,8 +11,9 @@ import DisplaySelectedGuests from "../components/DisplaySelectedGuests";
 export default function Recipes() {
   const { guestArray } = useContext(UserContext);
   let intolerances = getIntolerances(guestArray);
-  const [data, setData] = useState([]);
+  const [recipeData, setRecipeData] = useState([]);
   const [availableData, setAvailableData] = useState(true);
+  const [nextPage, setNextPage] = useState();
   const API_KEY = process.env.REACT_APP_API_KEY;
   const API_ID = process.env.REACT_APP_API_ID;
 
@@ -20,18 +22,36 @@ export default function Recipes() {
     const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${API_ID}&app_key=${API_KEY}${healthParams}`;
     const response = await fetch(url);
     const data = await response.json();
-    setData(data.hits);
+    setNextPage(data._links.next.href);
+    setRecipeData(data.hits);
     setAvailableData(data.hits.length > 0 ? true : false);
+  }
+
+  async function fetchDifferentPage(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    setNextPage(data._links.next.href);
+    setRecipeData(data.hits);
   }
 
   function onSubmit(event) {
     event.preventDefault();
-    setData([]);
+    setRecipeData([]);
     const form = event.target;
     const { recipeSearch } = form.elements;
     fetchData(recipeSearch.value);
     recipeSearch.value = "";
     recipeSearch.focus();
+  }
+
+  function ErrorCallback({ error, resetErrorBoundary }) {
+    return (
+      <StyledRecipeSection role="alert">
+        <p>Sorry, something went wrong. Error:</p>
+        <pre style={{ color: "red" }}>{error.message}</pre>
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </StyledRecipeSection>
+    );
   }
 
   return (
@@ -53,14 +73,22 @@ export default function Recipes() {
         />
         <button type="submit">Search</button>
       </form>
-      <StyledRecipeSection>
-        {data.map((recipe, index) => (
-          <RecipeCard key={index} recipeData={recipe} />
-        ))}
-        <StyledSorryMessage availableData={availableData}>
-          Sorry, we couldn't find any recipes... <br /> Please try again!
-        </StyledSorryMessage>
-      </StyledRecipeSection>
+      <ErrorBoundary
+        FallbackComponent={ErrorCallback}
+        onReset={() => setRecipeData([])}
+      >
+        <StyledRecipeSection>
+          {recipeData.map((recipe, index) => (
+            <RecipeCard key={index} recipeData={recipe} />
+          ))}
+          <StyledSorryMessage availableData={availableData}>
+            Sorry, we couldn't find any recipes... <br /> Please try again!
+          </StyledSorryMessage>
+          <button type="button" onClick={() => fetchDifferentPage(nextPage)}>
+            Next Page
+          </button>
+        </StyledRecipeSection>
+      </ErrorBoundary>
       <NavBar />
     </>
   );
@@ -73,7 +101,11 @@ const RecipeSearchLabel = styled.label`
 `;
 
 const StyledRecipeSection = styled.section`
+  margin-top: 20px;
   margin-bottom: 100px;
+  pre {
+    margin: 10px auto 20px auto;
+  }
 `;
 
 const StyledSorryMessage = styled.p`
