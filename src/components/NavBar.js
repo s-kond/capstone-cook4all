c52c9fd34c2022cf7fb1f115c62f745af5ab86dd
Stefan Kondring <kondring.s@gmx.de>
2022-11-07 12:12:38 +0100

    add info-modal, up-button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,27 +3,66 @@ import styled from "styled-components";
 import homeIcon from "../assets/icons/home-line.svg";
 import recipesIcon from "../assets/icons/hot-meal-outline.svg";
 import favoritesIcon from "../assets/icons/heart-outlined.svg";
+import upArrow from "../assets/icons/arrow-up-circle.svg";
+import { useEffect, useState } from "react";
+import InfoModal from "./InfoModal";
 
 export default function NavBar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <StyledNavBar>
-      <StyledNavLink to="/" end>
-        <img src={homeIcon} alt="Go to Home" />
-        <p>Home</p>
-      </StyledNavLink>
-      <StyledNavLink to="/recipes">
-        <img src={recipesIcon} alt="Go to Recipes" />
-        <p>Recipes</p>
-      </StyledNavLink>
-      <StyledNavLink to="/favorites">
-        <img src={favoritesIcon} alt="Go to favorite recipes" />
-        <p>Favorites</p>
-      </StyledNavLink>
-    </StyledNavBar>
+    <>
+      <StyledNavBar>
+        <StyledNavLink to="/" end>
+          <img src={homeIcon} alt="Go to Home" />
+          <p>Home</p>
+        </StyledNavLink>
+        <StyledNavLink to="/recipes">
+          <img src={recipesIcon} alt="Go to Recipes" />
+          <p>Recipes</p>
+        </StyledNavLink>
+        <StyledNavLink to="/favorites">
+          <img src={favoritesIcon} alt="Go to favorite recipes" />
+          <p>Favorites</p>
+        </StyledNavLink>
+        {showTopBtn && (
+          <StyledToTopButton type="button" onClick={() => goToTop()}>
+            <img src={upArrow} alt="arrow up" />
+          </StyledToTopButton>
+        )}
+        <StyledInfoButton
+          type="button"
+          title="intolerances, diets, ..."
+          onClick={() => setIsModalOpen(true)}
+        >
+          i
+        </StyledInfoButton>
+        <InfoModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+      </StyledNavBar>
+    </>
   );
 }
 
 const StyledNavBar = styled.nav`
+  position: relative;
   background-color: var(--primary-color);
   margin: 0 auto;
   width: 100%;
@@ -51,5 +90,37 @@ const StyledNavLink = styled(NavLink)`
   p {
     font-size: 0.9rem;
     color: black;
+  }
+`;
+
+const StyledInfoButton = styled.button`
+  position: absolute;
+  font-size: 1.1rem;
+  font-weight: bold;
+  padding: 5px 16px;
+  top: -50px;
+  right: 20px;
+  border-radius: 100%;
+  border: 2px solid black;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const StyledToTopButton = styled.button`
+  position: absolute;
+  right: 20px;
+  top: -100px;
+  height: 40px;
+  width: 40px;
+  border-radius: 100%;
+  padding: 0;
+  border: unset;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
   }
 `;
