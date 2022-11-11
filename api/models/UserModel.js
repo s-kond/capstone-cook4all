a7ce62773f50d215a85a6c022d7f4589417bb7dc
Stefan Kondring <kondring.s@gmx.de>
2022-11-11 17:35:49 +0100

    small addition

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -18,4 +18,4 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
