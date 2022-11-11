f19dddc8bede75b0c82e2f8f5db093b1df27279f
Stefan Kondring <kondring.s@gmx.de>
2022-11-11 10:39:46 +0100

    create routes to get/post user data

--- api/routes/Userdata.js
+++ api/routes/Userdata.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const User = require("../models/UserModel");
 
 const router = express.Router();
 
-router.get("/:username", async (req, res) => {
-  res.json({ msg: "GET the guestList from a user" });
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No user found under this id." });
+  }
+
+  const userData = await User.findById(id);
+
+  if (!userData) {
+    return res.status(404).json({ error: "Cannot find user." });
+  }
+  res.status(200).json(userData);
+});
+
+router.post("/", async (req, res) => {
+  const { name, guestList } = req.body;
+  try {
+    const user = await User.create({ name, guestList });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
