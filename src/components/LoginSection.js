6815b03ab3c4b242bb8546fb822d28884f5c9255
Stefan Kondring <kondring.s@gmx.de>
2022-11-12 13:23:21 +0100

    create loginSection
    - to send a GET-request for a specific username
    - give the possibility to login/logout

---
+++ src/components/LoginSection.js
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { UserContext } from "../util/UserContext";
+
+export default function LoginSection() {
+  const { fetchGuestList, username, setUsername, isLoggedIn, handleLogout } =
+    useContext(UserContext);
+  return (
+    <>
+      {isLoggedIn && <p>Hello {username}!</p>}
+      {isLoggedIn && (
+        <button type="button" onClick={() => handleLogout()}>
+          Logout
+        </button>
+      )}
+      {!isLoggedIn && (
+        <input
+          name="username"
+          id="username"
+          type="text"
+          //this pattern prevents users from submiting empty whitespace-filled names (from stackoverflow)
+          pattern=".*[^\s]{1,}.*"
+          placeholder="your username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      )}
+      {!isLoggedIn && (
+        <button type="button" onClick={() => fetchGuestList()}>
+          Login
+        </button>
+      )}
+    </>
+  );
+}
