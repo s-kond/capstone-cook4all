7b5fddbe42f5e7da6531278c494aa98ce2b2999f
Stefan Kondring <kondring.s@gmx.de>
2022-11-14 16:50:48 +0100

    add POST-Route to create new users
    export controller-functions

---
+++ api/controllers/userDataController.js
@@ -0,0 +1,50 @@
+const User = require("../models/UserModel");
+
+//get a single user
+const getUser = async (req, res) => {
+  const { username } = req.params;
+  const userData = await User.find({ name: username });
+  if (userData.length === 0) {
+    return res.status(404).json({ error: "No such user." });
+  }
+  res.status(200).json(userData);
+};
+
+//add a new user
+const addUser = async (req, res) => {
+  const { name, guestList, favoriteRecipes } = req.body;
+  const existingUser = await User.find({ name: name });
+  if (existingUser.length > 0) {
+    return res.status(400).json({ error: "This user already exists." });
+  }
+
+  try {
+    const user = await User.create({ name, guestList, favoriteRecipes });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//delete a user
+
+//update user data
+const updateUser = async (req, res) => {
+  const { guestList, favoriteRecipes } = req.body;
+  const { username } = req.params;
+  try {
+    const user = await User.findOneAndUpdate(
+      { name: username },
+      { guestList, favoriteRecipes }
+    );
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getUser,
+  addUser,
+  updateUser,
+};
--- api/routes/Userdata.js
+++ api/routes/Userdata.js
@@ -1,48 +1,16 @@
-const express = require("express");
-const User = require("../models/UserModel");
+const {
+  getUser,
+  addUser,
+  updateUser,
+} = require("../controllers/userDataController");
 
+const express = require("express");
 const router = express.Router();
 
-router.get("/all", async (req, res) => {
-  const userData = await User.find({});
+router.get("/:username", getUser);
 
-  if (!userData) {
-    return res.status(404).json({ error: "Cannot find any users." });
-  }
-  res.status(200).json(userData);
-});
+router.post("/addNew", addUser);
 
-router.get("/:username", async (req, res) => {
-  const { username } = req.params;
-  const userData = await User.find({ name: username });
-  if (userData.length === 0) {
-    return res.status(404).json({ error: "No such user." });
-  }
-  res.status(200).json(userData);
-});
-
-router.post("/", async (req, res) => {
-  const { name, guestList } = req.body;
-  try {
-    const user = await User.create({ name, guestList });
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-router.put("/:username", async (req, res) => {
-  const updates = req.body;
-  const { username } = req.params;
-  try {
-    const user = await User.findOneAndUpdate(
-      { name: username },
-      { guestList: updates }
-    );
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.put("/:username", updateUser);
 
 module.exports = router;
