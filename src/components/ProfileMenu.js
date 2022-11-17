636a822d0bd4fabc47a2d5ff5825ee5402a05a35
Stefan Kondring <kondring.s@gmx.de>
2022-11-17 09:19:24 +0100

    update menu style

--- src/components/ProfileMenu.js
+++ src/components/ProfileMenu.js
@@ -2,6 +2,10 @@ import { useContext } from "react";
 import { UserContext } from "../util/UserContext";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import deleteIcon from "../assets/icons/ei_trash.svg";
+import saveIcon from "../assets/icons/ic_round-save-alt.svg";
+import infoIcon from "../assets/icons/info-regular.svg";
+import logoutIcon from "../assets/icons/logout-outlined.svg";
 
 export default function ProfileMenu({
   isProfileMenuOpen,
@@ -10,7 +14,7 @@ export default function ProfileMenu({
 }) {
   const {
     isLoggedIn,
-    isChanges,
+    changesCounter,
     handleUserDataUpdate,
     handleDeleteUser,
     handleLogout,
@@ -35,12 +39,14 @@ export default function ProfileMenu({
           {!isLoggedIn && <button onClick={goToLogin}>Login</button>}
           {isLoggedIn && (
             <LogoutButton type="button" onClick={handleLogoutAndClose}>
-              Logout
+              <p>Logout</p>
+              <img src={logoutIcon} alt="delete" />
             </LogoutButton>
           )}
           {isLoggedIn && (
             <LogoutButton type="button" onClick={handleDeleteUser}>
-              Delete this account
+              <p>Delete this account</p>
+              <img src={deleteIcon} alt="delete" />
             </LogoutButton>
           )}
           <button
@@ -48,11 +54,14 @@ export default function ProfileMenu({
             title="intolerances, diets, ..."
             onClick={() => setIsInfoModalOpen(true)}
           >
-            Information
+            <p>Information</p>
+            <img src={infoIcon} alt="delete" />
           </button>
-          {isLoggedIn && isChanges && (
+          {isLoggedIn && changesCounter > 0 && (
             <button type="button" onClick={handleUserDataUpdate}>
-              Save changes !
+              <ChangeIcon>{changesCounter}</ChangeIcon>
+              <p>Save changes</p>
+              <img src={saveIcon} alt="delete" />
             </button>
           )}
         </ButtonContainer>
@@ -63,33 +72,52 @@ export default function ProfileMenu({
 
 const ButtonContainer = styled.div`
   position: absolute;
-  top: -200px;
+  top: -210px;
   right: 0;
   display: flex;
   flex-direction: column-reverse;
   align-items: flex-end;
+  gap: 10px;
   height: 200px;
   background-color: transparent;
-  width: 25%;
-
-  @media (max-width: 600px) {
-    width: 35%;
-  }
+  width: 50%;
 
   button {
     font-size: 1.1rem;
+    text-align: left;
     width: 100%;
     height: 2.5rem;
+    padding: 5px 20px 5px 30px;
     border: unset;
+    border-radius: 15px;
     background-color: var(--primary-color);
-    border-bottom: 1px solid black;
+    box-shadow: 1px 1px 2px black;
     cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    img {
+      width: 20px;
+    }
     &:hover {
       background-color: whitesmoke;
+    }
+    &:last-of-type {
+      position: relative;
     }
   }
 `;
 
 const LogoutButton = styled.button`
   color: red;
+`;
+
+const ChangeIcon = styled.p`
+  position: absolute;
+  top: -10px;
+  left: -10px;
+  padding: 3px 8px;
+  border-radius: 50px;
+  background-color: lightcoral;
+  color: white;
 `;
